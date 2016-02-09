488a5356e828ea84ca45b4d9b2136d921a06697b
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2016-02-09 10:04:11 -0800

    Add initial files

---
+++ src/emmet.js
@@ -0,0 +1,17 @@
+import LegoCharacter from "./LegoCharacter";
+
+// Here we use the extends keyword to make
+// Emmet inherit from LegoCharacter
+export default class Emmet extends LegoCharacter {
+  constructor() {
+    // super lets us call the LegoCharacter's constructor
+    super( { actor: "Chris Pratt", character: "Emmet" } );
+    this.sayings = [
+      "Introducing the double-decker couch!",
+      "So everyone can watch TV together and be buddies!",
+      "We're going to crash into the sun!",
+      "Hey, Abraham Lincoln, you bring your space chair right back!",
+      "Overpriced coffee! Yes!"
+    ];
+  }
+}
